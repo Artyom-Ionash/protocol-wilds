67a2dc7dadd52be36695824879a33a7456ba5c89
DukeSpontaneous <eai_bat@tut.by>
2025-12-13 20:17:30 +0300

    Исправил двойную привязку сигнала в пользу привязки в коде.

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-enum{
+enum {
 	MOVE,
 	ATTACK,
 	ATTACK2,
@@ -22,25 +22,27 @@ var attack_cooldown = false
 var player_pos
 var damage_basic = 10
 var damage_multiplier = 1
-var damage_current 
+var damage_current
 var recovery = false
 
 @onready var anim = $AnimatedSprite2D
 @onready var animPlayer = $AnimationPlayer
 @onready var stats = $stats # Предполагаем, что имя узла stats
 # Ссылки на UI компоненты, которыми владеет Игрок (или CanvasLayer внутри него)
-@onready var health_text = $HealthText 
+@onready var health_text = $HealthText
 @onready var health_anim = $HealthAnim
 
 func _ready() -> void:
 	add_to_group("player")
 	
-	# Подключаем сигналы от Stats к методам обновления UI
 	if stats:
-		stats.connect("health_changed", Callable(self, "_on_stats_health_changed"))
-		stats.connect("no_stamina", Callable(self, "_on_stats_no_stamina"))
+		stats.health_changed.connect(_on_stats_health_changed)
+		stats.no_stamina.connect(_on_stats_no_stamina)
+	else:
+		printerr("CRITICAL: Узел 'stats' не найден в Player!")
 	
-	Signals.connect('enemy_attack', Callable(self, '_on_damage_received'))
+	# Подключаем глобальный сигнал
+	Signals.enemy_attack.connect(_on_damage_received)
 	
 	# Инициализация прозрачности текста
 	if health_text:
@@ -62,19 +64,19 @@ func _physics_process(delta: float) -> void:
 		MOVE:
 			move_state()
 		ATTACK:
-			attack_state()	
+			attack_state()
 		ATTACK2:
-			attack2_state()	
+			attack2_state()
 		ATTACK3:
-			attack3_state()	
+			attack3_state()
 		BLOCK:
 			block_state()
 		SLIDE:
-			slide_state()	
+			slide_state()
 		DAMAGE:
-			damage_state()	
+			damage_state()
 		DEATH:
-			death_state()				
+			death_state()
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -90,56 +92,55 @@ func _physics_process(delta: float) -> void:
 	
 	damage_current = damage_basic * damage_multiplier
 	
-func move_state ():
+func move_state():
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED * run_speed
 		if velocity.y == 0:
-			if run_speed ==1:	
+			if run_speed == 1:
 				animPlayer.play('walk')
 			else:
-				animPlayer.play('Run')	
+				animPlayer.play('Run')
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0:
-			animPlayer.play('Idle')	
+			animPlayer.play('Idle')
 	if direction == -1:
 		$AnimatedSprite2D.flip_h = true
 		$AttackDirection.rotation_degrees = 180
 	elif direction == 1:
-		$AnimatedSprite2D.flip_h = false 	
-		$AttackDirection.rotation_degrees = 0	
+		$AnimatedSprite2D.flip_h = false
+		$AttackDirection.rotation_degrees = 0
 	if Input.is_action_pressed("run") and not recovery:
 		run_speed = 2
 		stats.stamina -= stats.run_cost
 	else:
-		run_speed = 1	
+		run_speed = 1
 	if Input.is_action_pressed("block"):
-		if not recovery:	
-			if velocity.x == 0 and stats.stamina >=1:
-				state = BLOCK		
+		if not recovery:
+			if velocity.x == 0 and stats.stamina >= 1:
+				state = BLOCK
 	if Input.is_action_pressed("slide") and velocity.x != 0:
-		if recovery == false:	
+		if recovery == false:
 			stats.stamina_cost = stats.slide_cost
-			if stats.stamina > stats.stamina_cost:	
+			if stats.stamina > stats.stamina_cost:
 				state = SLIDE
 	if Input.is_action_just_pressed('attack') and attack_cooldown == false:
-		if recovery == false:	
+		if recovery == false:
 			stats.stamina_cost = stats.attack_cost
 			if stats.stamina > stats.stamina_cost:
-				state = ATTACK 			
+				state = ATTACK
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		animPlayer.play("jump")			
+		animPlayer.play("jump")
 		
 func block_state():
-	
 	stats.stamina -= stats.block_cost
 	velocity.x = 0
-	animPlayer.play('block')	
+	animPlayer.play('block')
 	if Input.is_action_just_released('block') or recovery == true:
-		state = MOVE	
+		state = MOVE
 func slide_state():
 	stats.stamina_cost = stats.slide_cost
 	animPlayer.play('slide')
@@ -155,9 +156,9 @@ func death_state():
 func attack_state():
 	stats.stamina_cost = stats.attack_cost
 	damage_multiplier = 1
-	if Input.is_action_just_pressed('attack') and combo == true and stats.stamina > stats.stamina_cost:	 
+	if Input.is_action_just_pressed('attack') and combo == true and stats.stamina > stats.stamina_cost:
 		state = ATTACK2
-	velocity.x = 0	
+	velocity.x = 0
 	animPlayer.play('attack')
 	await animPlayer.animation_finished
 	attack_freeze()
@@ -168,14 +169,14 @@ func attack2_state():
 	damage_multiplier = 1.5
 	if Input.is_action_just_pressed('attack') and combo == true and stats.stamina > stats.stamina_cost:
 			state = ATTACK3
-	animPlayer.play('attack2')	
+	animPlayer.play('attack2')
 	await animPlayer.animation_finished
 	state = MOVE
 
 func attack3_state():
 	stats.stamina_cost = stats.attack_cost
 	damage_multiplier = 2
-	animPlayer.play('attack3')	
+	animPlayer.play('attack3')
 	await animPlayer.animation_finished
 	state = MOVE
 
@@ -208,21 +209,21 @@ func start_death_sequence():
 	SceneManager.load_menu()
 
 func _on_damage_received(enemy_damage):
-	if state == DEATH: return 
+	if state == DEATH: return
 	
 	if state == BLOCK:
 		enemy_damage /= 4
 	elif state == SLIDE:
 		enemy_damage = 0
 	else:
-		state = DAMAGE			
+		state = DAMAGE
 	
 	# Обращаемся к health через сеттер stats
 	stats.health -= enemy_damage
 	
 	if stats.health <= 0:
 		# Логику обнуления лучше держать в stats, но пока оставим здесь для совместимости
-		stats.health = 0 
+		stats.health = 0
 		start_death_sequence()
 
 func _on_hit_box_area_entered(_area: Area2D) -> void:
